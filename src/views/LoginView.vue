764f892cad8c9a471bb2c2e39e3ed2148c48ca33
QILLUAZ <6310210279@psu.ac.th>
2023-09-26 18:04:20 +0700

    frontend-vue

---
+++ src/views/LoginView.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-container>
+        <!-- v-row>
+            <v-col cols="1" v-for="i in 12" :key="i">
+                {{ 'col -' + i }}
+            </v-col>
+        </!-->
+        <v-row>
+            <v-col cols="12">
+                <LoginForm/>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+import LoginForm from '../components/Gologin.vue'
+export default {
+    components: {
+        LoginForm
+    }
+
+}
+</script>
+
+<style>
+
+</style>
